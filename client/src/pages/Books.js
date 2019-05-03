7b72346358bd546c9e28273bbc38b34e4c40f5b0
Matteo <mmarshburn@smu.edu>
2019-05-03 17:49:13 -0500

    53

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -27,18 +27,18 @@ class Books extends Component {
         <Row>
           <Col size="md-6">
             <Jumbotron>
-              <h1>What Books Should I Read?</h1>
+              <h1>Search for a book!</h1>
             </Jumbotron>
             <form>
               <Input name="title" placeholder="Title (required)" />
               <Input name="author" placeholder="Author (required)" />
               <TextArea name="synopsis" placeholder="Synopsis (Optional)" />
-              <FormBtn>Submit Book</FormBtn>
+              <FormBtn>Search</FormBtn>
             </form>
           </Col>
           <Col size="md-6 sm-12">
             <Jumbotron>
-              <h1>Books On My List</h1>
+              <h1>My Saved Books</h1>
             </Jumbotron>
             {this.state.books.length ? (
               <List>
